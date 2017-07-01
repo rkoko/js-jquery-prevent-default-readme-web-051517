bfb37726c17614ccb891d173355cb4bc6572b62e
rkoko <rinda.ko@gmail.com>
2017-07-01 16:33:50 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-  
-  // browser refreshes on submit
-  $('form').on('submit', function(){
-    var name = $('#name').val();
-    $("#hello").text("Hello, " + name);
-  });
 
-  //examine event object
+  // browser refreshes on submit
+  // $('form').on('submit', function(){
+  //   var name = $('#name').val();
+  //   $("#hello").text("Hello, " + name);
+  // });
+
+  // // examine event object
   // $('form').on('submit', function(event){
+  //   debugger
   //   var name = $('#name').val();
   //   $("#hello").text("Hello, " + name);
   //   debugger;
@@ -15,10 +16,10 @@ $(document).ready(function(){
   // });
 
   // stop page refresh
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   event.preventDefault();
-  // });
+  $('form').on('submit', function(event){
+    var name = $('#name').val();
+    $("#hello").text("Hello, " + name);
+    event.preventDefault();
+  });
 
 });
